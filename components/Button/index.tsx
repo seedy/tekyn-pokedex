e6513cca449f9c05a41f33db4261a74578965a46
Seedy <seedy.dupuis@gmail.com>
2022-05-01 14:15:58 +0200

    feat: Button

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,3 +1,79 @@
 import { styled } from "stitches.config";
 
-const Button = styled("button", {});
+const Button = styled("button", {
+  borderRadius: "$card",
+  background: "$primary",
+  width: "$7",
+  height: "$3",
+  filter: "drop-shadow($2)",
+  variants: {
+    variant: {
+      normal: {
+        background: "$normal",
+      },
+      fire: {
+        background: "$fire",
+      },
+      water: {
+        background: "$water",
+      },
+      grass: {
+        background: "$grass",
+      },
+      electrique: {
+        background: "$electrique",
+      },
+      ice: {
+        background: "$ice",
+      },
+      fight: {
+        background: "$fight",
+      },
+      poison: {
+        background: "$poison",
+      },
+      ground: {
+        background: "$ground",
+      },
+      flight: {
+        background: "$flight",
+      },
+      psychic: {
+        background: "$psychic",
+      },
+      bug: {
+        background: "$bug",
+      },
+      rock: {
+        background: "$rock",
+      },
+      ghost: {
+        background: "$ghost",
+      },
+      dark: {
+        background: "$dark",
+      },
+      dragon: {
+        background: "$dragon",
+      },
+      steel: {
+        background: "$steel",
+      },
+      fairy: {
+        background: "$fairy",
+      },
+    },
+    type: {
+      button: {
+        borderRadius: "$button",
+        filter: "none",
+      },
+      iconButton: {
+        borderRadius: "$iconButton",
+        filter: "none",
+      },
+    },
+  },
+});
+
+export default Button;
