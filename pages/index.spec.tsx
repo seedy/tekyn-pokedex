322f905f902113dc54250f3205f77e8b697e5af1
Seedy <seedy.dupuis@gmail.com>
2022-05-01 18:46:32 +0200

    chore: page extension to allow colocating tests

--- pages/index.spec.tsx
+++ pages/index.spec.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import MainPage from "pages/index";
+import MainPage from "pages/index.page";
 
 describe("main", () => {
   it("should render", () => {
