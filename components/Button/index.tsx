23347ea0266a9ec2b1b9c4297daf72a0b735017c
Seedy <seedy.dupuis@gmail.com>
2022-05-01 18:00:33 +0200

    chore: Button, apply styles

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -33,19 +33,50 @@ const makeButtonVariants = () =>
   );
 
 const Button = styled("button", {
+  position: "relative",
+  border: "none",
   borderRadius: "$card",
   background: "$primary",
   width: "$7",
   height: "$3",
-  filter: "drop-shadow($2)",
+  filter: "drop-shadow($shadows$2)",
+  fontFamily: "$roboto",
+  fontStyle: "normal",
+  fontWeight: "$bold",
+  fontSize: "$2",
+  lineHeight: "$2",
+  "&::before": {
+    content: "''",
+    position: "absolute",
+    inset: 0,
+    borderRadius: "inherit",
+  },
+  "&:hover": {
+    cursor: "pointer",
+    "&::before": {
+      backgroundColor: "$hoverBackdrop",
+    },
+  },
+  "&:focus": {
+    outline: "2px solid $outline",
+  },
+  "&:active": {
+    "&::before": {
+      backgroundColor: "$backdrop",
+    },
+  },
   variants: {
     variant: makeButtonVariants(),
     type: {
       button: {
+        width: "$9",
+        height: "$4",
         borderRadius: "$button",
         filter: "none",
       },
       iconButton: {
+        width: "$4",
+        height: "$4",
         borderRadius: "$iconButton",
         filter: "none",
       },
