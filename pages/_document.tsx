3d7ddcc1de58320ef24ab1bfe5022bfc75a63440
Seedy <seedy.dupuis@gmail.com>
2022-05-01 13:47:23 +0200

    chore: load roboto font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,10 @@ export default function Document() {
           id="stitches"
           dangerouslySetInnerHTML={{ __html: getCssText() }}
         />
+        <link
+          href="https://fonts.googleapis.com/css?family=Roboto&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
