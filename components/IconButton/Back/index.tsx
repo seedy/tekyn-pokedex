b314fa3046e4f836cb64d6c863926d1be565056a
Seedy <seedy.dupuis@gmail.com>
2022-05-03 23:58:19 +0200

    fix: IconButtonBack, negative margin for icon alignment, pass down css merged

--- components/IconButton/Back/index.tsx
+++ components/IconButton/Back/index.tsx
@@ -12,7 +12,7 @@ import {
 const IconButtonBack = forwardRef<
   ElementRef<typeof IconButton>,
   Omit<ComponentProps<typeof IconButton>, "children">
->(({ onClick, ...props }, ref) => {
+>(({ onClick, css, ...props }, ref) => {
   const { push } = useRouter();
 
   const handleClick: MouseEventHandler<HTMLButtonElement> = useCallback(
@@ -31,7 +31,11 @@ const IconButtonBack = forwardRef<
       aria-label="Back"
       onClick={handleClick}
       {...props}
-      css={{ background: "transparent" }}
+      css={{
+        background: "transparent",
+        marginLeft: "calc((24px - 65px) / 2)",
+        ...css,
+      }}
     >
       <BackArrow />
     </IconButton>
