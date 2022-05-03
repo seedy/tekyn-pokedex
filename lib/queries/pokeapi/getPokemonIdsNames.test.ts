6405bba1db683f2aadaa9bd50bee378db58f7cf6
Seedy <seedy.dupuis@gmail.com>
2022-05-03 21:43:14 +0200

    fix: query pokemon ids and names

--- lib/queries/pokeapi/getPokemonIdsNames.test.ts
+++ lib/queries/pokeapi/getPokemonIdsNames.test.ts
@@ -1,15 +1,15 @@
-import getPokemonIdsQuery, {Result} from "lib/queries/pokeapi/getPokemonIds";
-import mock from 'mocks/data/getPokemonIds.json'
+import getPokemonIdsNamesQuery, {Result} from "lib/queries/pokeapi/getPokemonIdsNames";
+import mock from 'mocks/data/getPokemonIdsNames.json'
 
 describe('pokeapi getPokemonIds', () => {
   it('should not throw', async () => {
-    await expect(getPokemonIdsQuery()).resolves.not.toThrow();
+    await expect(getPokemonIdsNamesQuery()).resolves.not.toThrow();
   })
   describe('result', () => {
     let result: Result;
     let mockData: Result
     beforeAll(async() => {
-      result = await getPokemonIdsQuery(10);
+      result = await getPokemonIdsNamesQuery(10);
       mockData = mock['10'];
     })
 
--- mocks/handlers/getPokemonIdsNames.ts
+++ mocks/handlers/getPokemonIdsNames.ts
@@ -1,11 +1,11 @@
-import mock from '../data/getPokemonIds.json'
+import mock from '../data/getPokemonIdsNames.json'
 import { DefaultRequestBody, ResponseComposition, RestContext, RestRequest } from 'msw';
 
 const handler = (req: RestRequest<DefaultRequestBody>, res: ResponseComposition<DefaultRequestBody>, ctx: RestContext) => {
   const {variables} = JSON.parse(req.body as string);
   const {limit} = variables;
   if (limit && limit < 10) {
-    const limitedIds = mock['10'].data.ids.slice(0, Number(limit));
+    const limitedIds = mock['10'].data.idsNames.slice(0, Number(limit));
     return res(
       ctx.json({
         data: {
