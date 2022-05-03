ae0df2e463c3bac814d6c8fc11c6a83e8119a1a6
Seedy <seedy.dupuis@gmail.com>
2022-05-04 01:19:00 +0200

    test: update homepage test, pokemonMain data model

--- components/Pokemon/Main/index.test.tsx
+++ components/Pokemon/Main/index.test.tsx
@@ -3,35 +3,27 @@ import PokemonMain from "components/Pokemon/Main";
 import { render } from "@testing-library/react";
 
 describe("PokemonMain", () => {
+  const TYPES = [
+    { type: { name: "fire" } },
+    { type: { name: "flight" } },
+  ] as Type[];
   it("should render a single image", async () => {
     const { queryByRole } = render(
-      <PokemonMain
-        id={6}
-        name="Charizard"
-        types={[{ name: "fire" }, { name: "flight" }]}
-      />
+      <PokemonMain id={6} name="Charizard" types={TYPES} />
     );
 
     expect(queryByRole("img")).toBeInTheDocument();
   });
   it("should render a PokemonMain with alt text", async () => {
     const { queryByAltText } = render(
-      <PokemonMain
-        id={6}
-        name="Charizard"
-        types={[{ name: "fire" }, { name: "flight" }]}
-      />
+      <PokemonMain id={6} name="Charizard" types={TYPES} />
     );
 
     expect(queryByAltText("#006 - Charizard")).toBeInTheDocument();
   });
   it("should render a PokemonMain with 2 buttons for type", async () => {
     const { queryAllByRole } = render(
-      <PokemonMain
-        id={6}
-        name="Charizard"
-        types={[{ name: "fire" }, { name: "flight" }]}
-      />
+      <PokemonMain id={6} name="Charizard" types={TYPES} />
     );
     expect(queryAllByRole("button")).toHaveLength(2);
   });
