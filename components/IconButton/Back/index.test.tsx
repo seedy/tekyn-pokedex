2797c00bf1e67a23aaaae658af93697c1b9bd3cd
Seedy <seedy.dupuis@gmail.com>
2022-05-03 23:15:57 +0200

    feat: IconButtonBack, add navigation (#35)

--- components/IconButton/Back/index.test.tsx
+++ components/IconButton/Back/index.test.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import IconButtonBack from "components/IconButton/Back";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
+import mockRouter from "next-router-mock";
+import { act } from "react-dom/test-utils";
+
+jest.mock("next/router", () => require("next-router-mock"));
 
 describe("IconButtonBack", () => {
+  beforeEach(() => {
+    mockRouter.setCurrentUrl("/6");
+  });
   it("should render a IconButtonBack", async () => {
     const { queryByRole } = render(<IconButtonBack />);
 
@@ -12,5 +19,16 @@ describe("IconButtonBack", () => {
     const { queryByLabelText } = render(<IconButtonBack />);
 
     expect(queryByLabelText("Back")).toBeInTheDocument();
+  });
+  it("should navigate back to root path", async () => {
+    const { getByRole } = render(<IconButtonBack />);
+
+    const button = getByRole("button");
+    expect(button).toBeInTheDocument();
+
+    act(() => {
+      fireEvent.click(button);
+    });
+    expect(mockRouter.asPath).toBe("/");
   });
 });
--- components/IconButton/Back/index.tsx
+++ components/IconButton/Back/index.tsx
@@ -1,20 +1,42 @@
 import IconButton from "components/IconButton";
 import BackArrow from "components/icons/BackArrow";
-import { ComponentProps, ElementRef, forwardRef } from "react";
+import { useRouter } from "next/router";
+import {
+  ComponentProps,
+  ElementRef,
+  forwardRef,
+  MouseEventHandler,
+  useCallback,
+} from "react";
 
 const IconButtonBack = forwardRef<
   ElementRef<typeof IconButton>,
   Omit<ComponentProps<typeof IconButton>, "children">
->((props, ref) => (
-  <IconButton
-    ref={ref}
-    aria-label="Back"
-    {...props}
-    css={{ background: "transparent" }}
-  >
-    <BackArrow />
-  </IconButton>
-));
+>(({ onClick, ...props }, ref) => {
+  const { push } = useRouter();
+
+  const handleClick: MouseEventHandler<HTMLButtonElement> = useCallback(
+    (e) => {
+      if (onClick) {
+        onClick(e);
+      }
+      push("/");
+    },
+    [push, onClick]
+  );
+
+  return (
+    <IconButton
+      ref={ref}
+      aria-label="Back"
+      onClick={handleClick}
+      {...props}
+      css={{ background: "transparent" }}
+    >
+      <BackArrow />
+    </IconButton>
+  );
+});
 IconButtonBack.displayName = "IconButtonBack";
 
 export default IconButtonBack;
