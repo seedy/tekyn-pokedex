0c0778a322cd505b82074c24028faf543671883c
Seedy <seedy.dupuis@gmail.com>
2022-05-04 01:19:26 +0200

    38 fix unit tests (#39)
    * chore: jest, mock next dynamic
    
    * fix: homepage, suspense no ssr, typings
    
    * test: update homepage test, pokemonMain data model

--- jest.setup.js
+++ jest.setup.js
@@ -14,3 +14,12 @@ if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
   
   afterAll(() => server.close())
 }
+
+// Manually mock next/dynamic as the next.js (7.0.2) babel plugin will compile to Webpack
+// lazy imports (require.resolveWeak) who're conflicting with the Node module system.
+jest.mock('next/dynamic', () => () => {
+  const DynamicComponent = () => null;
+  DynamicComponent.displayName = 'LoadableComponent';
+  DynamicComponent.preload = jest.fn();
+  return DynamicComponent;
+});
--- pages/[[...args]].spec.tsx
+++ pages/[[...args]].spec.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import MainPage from "pages/[[...args]].page";
+import mockRouter from "next-router-mock";
 jest.mock("next/router", () => require("next-router-mock"));
 
 describe("main", () => {
+  beforeEach(() => {
+    mockRouter.setCurrentUrl("/");
+  });
   it("should render", () => {
-    const { queryByText } = render(<MainPage />);
+    const { queryByText } = render(<MainPage pokemonIdsNames={[]} />);
 
-    expect(queryByText("Welcome")).toBeInTheDocument();
+    expect(queryByText("Search !")).toBeInTheDocument();
   });
 });
